d37858639ce017852d7880a460f54ed4a97c4162
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-09-26 16:53:19 +0700

    Add files via upload

---
+++ PBO1-10118006-Latihan12-Aritmatika/src/pbo1/pkg10118006/latihan12/aritmatika/PBO110118006Latihan12Aritmatika.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118006.latihan12.aritmatika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : PBO1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini berisi  program untuk aritmatika
+ */
+public class PBO110118006Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b =" + (a + b));
+        System.out.println("a - b =" + (a - b));
+        System.out.println("a * b =" + (a * b));
+        System.out.println("b / a =" + (b / a));
+        System.out.println("b % a =" + (b % a));
+        System.out.println("c % a =" + (c % a));
+        System.out.println("a++ =" + (a++));
+        System.out.println("a-- =" + (a--));
+        System.out.println("d++ =" + (d++));
+        System.out.println("++d =" + (++d));
+        
+        
+    }
+    
+}
